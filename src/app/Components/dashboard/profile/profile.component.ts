4ccd8a8b0cae2cc49f8f040a2811626135fb1116
PE <pouya.ebrahimi4979@gmail.com>
2021-07-23 16:18:52 +0430

    Profile

--- src/app/Components/dashboard/profile/profile.component.ts
+++ src/app/Components/dashboard/profile/profile.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  leftdays: number = 25;
 }
