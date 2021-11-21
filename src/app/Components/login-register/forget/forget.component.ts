24000fe419fd613f482a15530f74209eca6b01fc
Pouya Ebraihimi <pouyaebrahimi.business@gmail.com>
2021-11-21 16:06:27 +0330

    Fixed forget and register forms to send data

--- src/app/Components/login-register/forget/forget.component.ts
+++ src/app/Components/login-register/forget/forget.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { EnrollmentService } from 'src/app/Services/enrollment.service';
 
 @Component({
@@ -9,20 +10,21 @@ import { EnrollmentService } from 'src/app/Services/enrollment.service';
 })
 export class ForgetComponent{
 
-  constructor(private enroll: EnrollmentService, private builder: FormBuilder) { }
+  constructor(private enroll: EnrollmentService, private builder: FormBuilder,private _dialog: MatDialogRef<ForgetComponent>) { }
   @Output() page: EventEmitter<string> = new EventEmitter();
   forgetForm: FormGroup = this.builder.group({
     email: ['',Validators.required]
   })
   onSubmit(){
-    this.enroll.forget(this.forgetForm.value).subscribe({
-      next: (data: any) => {
-        console.log(data);
-      },
-      error: (err: any) => {
-        console.error(err);
-      }
-    })
+    this._dialog.close(this.forgetForm.value);
+    // this.enroll.forget(this.forgetForm.value).subscribe({
+    //   next: (data: any) => {
+    //     console.log(data);
+    //   },
+    //   error: (err: any) => {
+    //     console.error(err);
+    //   }
+    // })
   }
   navigate(page: string){
     this.page.emit(page);
--- src/app/Components/login-register/register/register.component.ts
+++ src/app/Components/login-register/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { EnrollmentService } from 'src/app/Services/enrollment.service';
 import { checkPassword } from './Custome';
 
@@ -10,7 +11,7 @@ import { checkPassword } from './Custome';
 })
 export class RegisterComponent{
   
-  constructor(private enroll: EnrollmentService, private builder: FormBuilder){}
+  constructor(private enroll: EnrollmentService, private builder: FormBuilder,private _dialog: MatDialogRef<RegisterComponent>){}
   @Output() page: EventEmitter<string> = new EventEmitter();
   registerForm = this.builder.group({
     fullname: ['', Validators.required],
@@ -19,14 +20,15 @@ export class RegisterComponent{
     confirmPassword: ['', Validators.required]
   }, {validator: checkPassword})
   onSubmit(){
-    this.enroll.register(this.registerForm.value).subscribe({
-      next: (data: any) => {
-        console.log(data);
-      },
-      error: (err: any) => {
-        console.error(err)
-      }
-    })
+    this._dialog.close(this.registerForm.value);
+    // this.enroll.register(this.registerForm.value).subscribe({
+    //   next: (data: any) => {
+    //     console.log(data);
+    //   },
+    //   error: (err: any) => {
+    //     console.error(err)
+    //   }
+    // })
   }
   navigate(page: string){
     this.page.emit(page);
