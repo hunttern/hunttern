31137f5c57961eb23bd3e8bb4532091a77a26ce3
Pouya Ebraihimi <pouyaebrahimi.business@gmail.com>
2021-11-23 11:51:51 +0000

    check if position exist then draw - watchlist selected

--- src/app/Components/windows-platform/tools/whatchlist/list/list.component.ts
+++ src/app/Components/windows-platform/tools/whatchlist/list/list.component.ts
@@ -6,9 +6,16 @@ import { patternClass } from '../../../../../Patterns/Patterns.class';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  selectedRow: number = 0;
+  coins: Array<{symbol: string,last: number, change: number, vol: number}> = [
+    {symbol: 'BTCUSDT',change: 1301.05, last: 5.65,vol: 125.71},
+    {symbol: 'ETHUSDT',change: 1301.05, last: 5.65,vol: 125.71},
+    {symbol: 'ADAUSDT',change: 1301.05, last: 5.65,vol: 125.71}
+  ]
 
-  getSymbolChart(symbol: string){
+  getSymbolChart(symbol: string, row: number){
     const chart =   patternClass.chart;
+    this.selectedRow = row;
     chart.setSymbol(symbol);
   }
 }
--- src/app/Patterns/Services/Position/position.service.ts
+++ src/app/Patterns/Services/Position/position.service.ts
@@ -17,62 +17,63 @@ export class PositionService {
   }
 
   drawPosition(){
-
-    const prz: number[] = this._position[0].PRZ;
-    const entry: any = this._position[0].Entry;
-    const SL: number = this._position[0].StopLosses[0];
-    const TP1: number = this._position[0].TargetPoints[0];
-    const TP2: number = this._position[0].TargetPoints[1];
-    const type: string = this._position[0].PositionType[0];
-    // const price = entry;
-    // const time = (Date.parse(entry.Time) / 1000);
-    const entryLineID = this._chart.createMultipointShape([{price: entry}],
-      {
-        shape: "horizontal_line",
-        lock: true,
-        disableSelection: true,
-        disableSave: true,
-        disableUndo: true,
-        overrides: {
-          linestyle: 2,
-          linecolor: '#ffff00'
-        }
-      });
-    const SLLineID = this._chart.createMultipointShape([{price: SL}],
-      {
-        shape: "horizontal_line",
-        lock: true,
-        disableSelection: true,
-        disableSave: true,
-        disableUndo: true,
-        overrides: {
-          linestyle: 2,
-          linecolor: '#ff0000'
-        }
-      });
-    const TP1LineID = this._chart.createMultipointShape([{price: TP1}],
-      {
-        shape: "horizontal_line",
-        lock: true,
-        disableSelection: true,
-        disableSave: true,
-        disableUndo: true,
-        overrides: {
-          linestyle: 2,
-          linecolor: '#00ff00'
-        }
-      });
-    const TP2LineID = this._chart.createMultipointShape([{price: TP2}],
-      {
-        shape: "horizontal_line",
-        lock: true,
-        disableSelection: true,
-        disableSave: true,
-        disableUndo: true,
-        overrides: {
-          linestyle: 2,
-          linecolor: '#00ff00'
-        }
-      });
+    if(this._position.length > 0){
+      const prz: number[] = this._position[0].PRZ;
+      const entry: any = this._position[0].Entry;
+      const SL: number = this._position[0].StopLosses[0];
+      const TP1: number = this._position[0].TargetPoints[0];
+      const TP2: number = this._position[0].TargetPoints[1];
+      const type: string = this._position[0].PositionType[0];
+      // const price = entry;
+      // const time = (Date.parse(entry.Time) / 1000);
+      const entryLineID = this._chart.createMultipointShape([{price: entry}],
+        {
+          shape: "horizontal_line",
+          lock: true,
+          disableSelection: true,
+          disableSave: true,
+          disableUndo: true,
+          overrides: {
+            linestyle: 2,
+            linecolor: '#ffff00'
+          }
+        });
+      const SLLineID = this._chart.createMultipointShape([{price: SL}],
+        {
+          shape: "horizontal_line",
+          lock: true,
+          disableSelection: true,
+          disableSave: true,
+          disableUndo: true,
+          overrides: {
+            linestyle: 2,
+            linecolor: '#ff0000'
+          }
+        });
+      const TP1LineID = this._chart.createMultipointShape([{price: TP1}],
+        {
+          shape: "horizontal_line",
+          lock: true,
+          disableSelection: true,
+          disableSave: true,
+          disableUndo: true,
+          overrides: {
+            linestyle: 2,
+            linecolor: '#00ff00'
+          }
+        });
+      const TP2LineID = this._chart.createMultipointShape([{price: TP2}],
+        {
+          shape: "horizontal_line",
+          lock: true,
+          disableSelection: true,
+          disableSave: true,
+          disableUndo: true,
+          overrides: {
+            linestyle: 2,
+            linecolor: '#00ff00'
+          }
+        });
+    }
   }
 }
