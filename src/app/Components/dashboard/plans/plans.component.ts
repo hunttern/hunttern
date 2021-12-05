dbb8a436cdc880c3a0b44499cf9eb85f96c87151
Pouya Ebraihimi <pouyaebrahimi.business@gmail.com>
2021-12-05 06:53:42 +0000

    Plans

--- src/app/Components/dashboard/plans/plans.component.ts
+++ src/app/Components/dashboard/plans/plans.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plans.component.scss']
 })
 export class PlansComponent implements OnInit {
+  leftDays: number = 27;
+  goneDays: number = 3;
+  data: any = [  {
+    "name": "left",
+    "value": this.leftDays
+  },
+  {
+    "name": "gone",
+    "value": this.goneDays
+  }]
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
 
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+  single: any[];
+  view: any[] = ['700', '400'];
   constructor() { }
 
   ngOnInit(): void {
