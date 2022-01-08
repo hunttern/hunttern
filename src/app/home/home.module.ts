6964168f4841a1a797102c25499baedc7886c69c
Pouya Ebraihimi <pouyaebrahimi.business@gmail.com>
2022-01-08 19:17:13 +0330

    removed home components to replace with mew ones

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,16 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FeaturesComponent } from './Components/features/features.component';
-import { PackagesComponent } from './Components/packages/packages.component';
-import { ServicesComponent } from './Components/services/services.component';
-import { SubscribeComponent } from './Components/subscribe/subscribe.component';
 import { SharedModule } from 'src/app/Shared/shared.module';
 
 
 @NgModule({
-  declarations: [HomeComponent,HeaderComponent,FeaturesComponent,PackagesComponent,ServicesComponent,SubscribeComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     SharedModule,
