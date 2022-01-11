f19c5d39404d881dd76b8b29b30bf2d78f6c8b36
Pouya Ebraihimi <pouyaebrahimi.business@gmail.com>
2022-01-11 17:02:25 +0330

    FAQ

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,22 +4,26 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { MaterialsModule } from 'src/app/Shared/materials/materials.module';
+import { SharedModule } from '../Shared/shared.module';
+
 import { HomeComponent } from './home.component';
-import { SharedModule } from 'src/app/Shared/shared.module';
 import { HeaderComponent } from './Components/header/header.component';
 import { ServicesComponent } from './Components/services/services.component';
 import { ServiceComponent } from './Components/services/service/service.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { MarketsComponent } from './components/markets/markets.component';
-import { SubscribeComponent } from './components/subscribe/subscribe.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { MarketsComponent } from './Components/markets/markets.component';
+import { SubscribeComponent } from './Components/subscribe/subscribe.component';
+import { FAQComponent } from './Components/faq/faq.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, ServicesComponent, ServiceComponent, AboutUsComponent, MarketsComponent, SubscribeComponent],
+  declarations: [HomeComponent, HeaderComponent, ServicesComponent, ServiceComponent, AboutUsComponent, MarketsComponent, SubscribeComponent, FAQComponent],
   imports: [
     CommonModule,
-    SharedModule,
     RouterModule,
+    MaterialsModule,
+    SharedModule,
     ReactiveFormsModule
   ],
   exports: [HomeComponent]
